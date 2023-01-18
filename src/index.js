3ae865c8b7e0c434779a0465f13f1323348a5db7
Ziru Zou <jiyucoding@yahoo.com>
2023-01-18 10:10:18 -0500

    load images on DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 // write your code here
+fetch("http://localhost:3000/ramens")
+.then(response => response.json())
+.then(ramenData => ramenData.forEach(ramen => renderRamen(ramen)))
+
+let ramenMenu = document.querySelector("#ramen-menu")
+let ramenDetail = document.querySelector("#ramen-detail")
+let ramenImg = document.querySelector("#detail-image")
+let ramenName = ramenDetail.querySelector(".name")
+let ramenRestaurant = ramenDetail.querySelector(".restaurent")
+let ramenRating = document.querySelector("#rating-display")
+let ramenComment = document.querySelector("#comment-display")
+
+
+// ramenName.innerHTML = ramen.name
+// ramenRestaurant.innerHTML = ramen.restaurant
+// ramenRating.innerHTML = ramen.rating
+// ramenComment.innerHTML = ramen.comment
+
+function renderRamen(ramen) {
+    let img = document.createElement("img")
+    img.src = ramen.image
+    console.log(img)
+    console.log(img.src)
+    ramenMenu.append(img)
+
+}
