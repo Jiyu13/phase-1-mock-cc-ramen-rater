010178eb84f82f10e5b302f669f3bd99b1402836
Ziru Zou <jiyucoding@yahoo.com>
2023-01-18 11:02:53 -0500

    click image and show on image detail container

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 // write your code here
-fetch("http://localhost:3000/ramens")
-.then(response => response.json())
-.then(ramenData => ramenData.forEach(ramen => renderRamen(ramen)))
-
 let ramenMenu = document.querySelector("#ramen-menu")
 let ramenDetail = document.querySelector("#ramen-detail")
-let ramenImg = document.querySelector("#detail-image")
+
+let ramenImg = document.querySelector(".detail-image")
 let ramenName = ramenDetail.querySelector(".name")
-let ramenRestaurant = ramenDetail.querySelector(".restaurent")
+let ramenRestaurant = ramenDetail.querySelector(".restaurant")
 let ramenRating = document.querySelector("#rating-display")
 let ramenComment = document.querySelector("#comment-display")
 
+function getALLRamen() {
+    fetch("http://localhost:3000/ramens")
+    .then(response => response.json())
+    .then(ramenData => ramenData.forEach(ramen => renderRamens(ramen)))
+}
+getALLRamen()
 
-// ramenName.innerHTML = ramen.name
-// ramenRestaurant.innerHTML = ramen.restaurant
-// ramenRating.innerHTML = ramen.rating
-// ramenComment.innerHTML = ramen.comment
 
-function renderRamen(ramen) {
+function renderRamens(ramen) {
     let img = document.createElement("img")
     img.src = ramen.image
-    console.log(img)
-    console.log(img.src)
+    img.id = ramen.id
+    img.addEventListener("click", () => {
+        ramenImg.src = ramen.image
+        ramenName.innerHTML = ramen.name
+        ramenRestaurant.innerHTML = ramen.restaurant
+        ramenRating.innerHTML = ramen.rating
+        ramenComment.innerHTML = ramen.comment
+    })
     ramenMenu.append(img)
-
 }
+
+
+
